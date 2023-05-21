3c72fee499d761e94d16237d2a690f1c36bebceb
Richard Lois <richard.lois@ti.ukdw.ac.id>
2023-05-21 20:25:48 +0700

    Init fragment and navigation

---
+++ TechMate/app/src/main/java/id/co/ukdw/techmate/ui/SplashScreenFragment.kt
@@ -0,0 +1,23 @@
+package id.co.ukdw.techmate.ui
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import id.co.ukdw.techmate.R
+
+@SuppressLint("CustomSplashScreen")
+class SplashScreenFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_splash_screen, container, false)
+    }
+
+
+}
