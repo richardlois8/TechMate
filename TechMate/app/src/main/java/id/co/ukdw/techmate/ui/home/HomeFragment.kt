f4dafc714760e688965acaea4840eb6e21adc068
Richard Lois <richard.lois@ti.ukdw.ac.id>
2023-05-22 14:31:26 +0700

    Setup recycler view on home fragment

--- TechMate/app/src/main/java/id/co/ukdw/techmate/ui/home/HomeFragment.kt
+++ TechMate/app/src/main/java/id/co/ukdw/techmate/ui/home/HomeFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.GridLayoutManager
 import id.co.ukdw.techmate.MainActivity
 import id.co.ukdw.techmate.R
 import id.co.ukdw.techmate.databinding.FragmentHomeBinding
@@ -23,7 +24,16 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setupRecyclerView()
         showBottomNav()
+    }
+
+    private fun setupRecyclerView() {
+        val dataGadget = (activity as MainActivity).getEngine().getAllGadget()
+        binding.apply {
+            recViewGadget.layoutManager = GridLayoutManager(context, 1)
+            recViewGadget.adapter = GadgetAdapter(dataGadget)
+        }
     }
 
     private fun showBottomNav() {
