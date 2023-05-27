79c7396793194b08c1a6b9dd8e55d6bde0b27e89
Richard Lois <richard.lois@ti.ukdw.ac.id>
2023-05-27 14:22:53 +0700

    Format price in detail

--- TechMate/app/src/main/java/id/co/ukdw/techmate/ui/DetailGadgetFragment.kt
+++ TechMate/app/src/main/java/id/co/ukdw/techmate/ui/DetailGadgetFragment.kt
@@ -8,6 +8,8 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.bumptech.glide.Glide
 import id.co.ukdw.techmate.databinding.FragmentDetailGadgetBinding
+import java.text.DecimalFormat
+import java.text.NumberFormat
 
 class DetailGadgetFragment : Fragment() {
     private var _binding: FragmentDetailGadgetBinding? = null
@@ -37,7 +39,10 @@ class DetailGadgetFragment : Fragment() {
         binding.detailDesc.text = gadgetCase.desc
         binding.memoriValue.text = gadgetCase.memory.toString()
         binding.ramValue.text = gadgetCase.ram.toString()
-        binding.price.text = gadgetCase.price.toString()
+
+        val formatter: NumberFormat = DecimalFormat("#,###")
+        val formattedNumber: String = formatter.format(gadgetCase.price)
+        binding.price.text = "Rp. $formattedNumber"
     }
 
     override fun onDestroyView() {
